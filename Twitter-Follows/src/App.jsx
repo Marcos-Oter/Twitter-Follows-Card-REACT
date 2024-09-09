1807de5eafd935739ddf0f35d42fac3800852128
Marcos-Oter <marcosotermin@hotmail.com>
2024-09-09 19:31:25 -0300

    Simular Fetch

--- Twitter-Follows/src/App.jsx
+++ Twitter-Follows/src/App.jsx
@@ -2,18 +2,47 @@ export { App }
 import { TwitterFollowCard } from "./TwitterFollowCard"
 import "./App.css"
 
+const users = [
+    {
+        userName: "NasukiChan",
+        isFollowing: false
+    },
+    {
+        userName: "SrSelian",
+        isFollowing: true
+    },
+    {
+        userName: "SutsukiChan_",
+        isFollowing: true
+    },
+    {
+        userName: "UanPreyas",
+        isFollowing : false
+    },
+    {
+        userName: "_Anandah",
+        isFollowing: false
+    },
+    {
+        userName: "x",
+        isFollowing: false
+    },
+]
+
 function App ()
 {
     return(
         <>
-        <aside className="tw-followCard-section">
-            <strong>A quién seguir</strong>
-            <TwitterFollowCard userName={"SrSelian"} initialIsFollowing></TwitterFollowCard>
-            <TwitterFollowCard userName={"SutsukiChan_"} initialIsFollowing></TwitterFollowCard>
-            <TwitterFollowCard userName={"UanPreyas"} initialIsFollowing={false}></TwitterFollowCard>
-            <TwitterFollowCard userName={"_Anandah"} initialIsFollowing={false}></TwitterFollowCard>
-            <TwitterFollowCard initialIsFollowing={false}></TwitterFollowCard>
-        </aside>
+            <aside className="tw-followCard-section">
+                <strong>A quién seguir</strong>
+                {
+                users.map(({userName, isFollowing}) => 
+                    (
+                        <TwitterFollowCard key={userName} userName={userName} initialIsFollowing={isFollowing}></TwitterFollowCard>
+                    )
+                )   
+                }
+            </aside>
         </>
     )
 }
